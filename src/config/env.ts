290f828966671ca226cb5f518e8f62747ba74b2b
BlendaAbdullahu <ba70455@ubt-uni.net>
2026-06-02 21:44:23 +0200

    Update Read me

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config({ quiet: true });
 
 export const env = {
-    port: Number(process.env.PORT || 3006),
+    port: Number(process.env.PORT || 3007),
     nodeEnv: process.env.NODE_ENV || 'development',
     databaseUrl: process.env.DATABASE_URL || '',
     jwtAccessSecret: process.env.JWT_ACCESS_SECRET || '',
